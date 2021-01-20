ace0f6be83a8d3b09499214d7cee223a215f88fd
Tomer Shlomo <tomer.s@claroty.com>
2021-01-20 11:56:45 +0200

    set package info to setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,48 @@
 
 from setuptools import setup, find_packages
 
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+
+def read(path):
+    return (HERE / path).read_text("utf-8").strip()
+
+
 install_requires = [
-    'aiohttp<4.0.0',
-    'aiofiles',
-    'pymongo',
-    'marshmallow',  # We have temporary backwards compatibility for 2.X, but also support 3.X
-    'cryptography',
-    'colorama',
-    'click'
+    'aiohttp>=3.5.4,<4.0.0',
+    'aiofiles>=0.0.4',
+    'pymongo<=3.11.2',
+    'marshmallow<=3.10.0',  # We have temporary backwards compatibility for 2.X, but also support 3.X
+    'cryptography>=2.8',
+    'colorama<=0.4.4',
+    'click<=7.2'
 ]
 
 setup(
     name="netunnel",
     version='1.0.2',
-    description='SSL tunnels utilities',
-    author='Claroty Ltd.',
-    author_email='pypi@claroty.com',
+    description='A tool to create network tunnels over HTTP/S written in Python 3',
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    author='Claroty Open Source',
+    author_email='opensource@claroty.com',
+    maintainer='Claroty Open Source',
+    maintainer_email='opensource@claroty.com',
+    url='https://github.com/claroty/netunnel',
+    license="Apache 2",
     packages=find_packages(exclude=('*test*',)),
     install_requires=install_requires,
     include_package_data=True,
     classifiers=[
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
