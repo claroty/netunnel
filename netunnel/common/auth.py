20fe08731f58bf54bfae1c93b3ec565f8bf6d32f
Tomer Shlomo <tomer.s@claroty.com>
2021-01-20 12:04:34 +0200

    raise NotImplementedError for auth base classes

--- netunnel/common/auth.py
+++ netunnel/common/auth.py
@@ -20,7 +20,7 @@ class NETunnelServerAuth(abc.ABC):
         async def get_client_for_peer(self, username, password):
             return NETunnelClientAuth(username, password)
         """
-        pass
+        raise NotImplementedError
 
     @abc.abstractmethod
     async def is_authenticated(self, request: web.Request) -> bool:
@@ -28,7 +28,7 @@ class NETunnelServerAuth(abc.ABC):
         Return True if the request is authorized and False if not.
         The headers of the request should include the return value of NETunnelClientAuth.get_authorized_headers()
         """
-        pass
+        raise NotImplementedError
 
     @abc.abstractmethod
     async def authenticate(self, request: web.Request):
@@ -38,7 +38,7 @@ class NETunnelServerAuth(abc.ABC):
         The NETunnelClientAuth.authenticate(client) should make a request to the /authenticate uri of the NETunnel server,
         and this method will be called as the route handler.
         """
-        pass
+        raise NotImplementedError
 
 
 class NETunnelClientAuth(abc.ABC):
@@ -55,7 +55,7 @@ class NETunnelClientAuth(abc.ABC):
         After calling this method, self.get_authorized_headers is expected to return valid headers which will
         be authorized by the remote.
         """
-        pass
+        raise NotImplementedError
 
     @abc.abstractmethod
     async def is_authenticated(self) -> bool:
@@ -63,7 +63,7 @@ class NETunnelClientAuth(abc.ABC):
         Return True if we are authenticated with the remote.
         This should return False if the tokens returned by the server are expired.
         """
-        pass
+        raise NotImplementedError
 
     @abc.abstractmethod
     async def get_authorized_headers(self) -> dict:
@@ -71,17 +71,18 @@ class NETunnelClientAuth(abc.ABC):
         Return headers which can be used to make authorized requests to the remote server.
         Raises NETunnelNotAuthenticatedError if we need to authenticate first/again
         """
-        pass
+        raise NotImplementedError
 
     @abc.abstractmethod
     def dump_object(self) -> dict:
         """
+        This method is not async since we're using marshmallow to parse this data and it does not support asyncio yet.
         Return a dump of this NETunnelClientAuth instance's data so it can be stored in the configuration
         and reloaded during startup. This is used by NETunnelServer when handling peer objects.
         The return value should be a dictionary that can be given to NETunnelServerAuth.get_client_for_peer() and
         will generate a valid NETunnelClientAuth object.
         """
-        pass
+        raise NotImplementedError
 
 
 class ServerNoAuth(NETunnelServerAuth):
